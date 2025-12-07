75dc1123c651d6899882dd417ca8b98590883c16
Jisu Hong <jaylovegood@snu.ac.kr>
2025-12-08 02:04:17 +0900

    swagger auth header 필드 이름 변경

--- src/main/kotlin/com/wafflestudio/spring2025/user/controller/UserController.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/user/controller/UserController.kt
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/users")
-@SecurityRequirement(name = "bearer")
+@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "User", description = "사용자 API")
 class UserController(
     private val userService: UserService,
